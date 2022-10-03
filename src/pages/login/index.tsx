402a56d12f06900a993a27cf108170fd5eddea9b
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-10-03 17:14:00 +0000

    Update index.tsx

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+
 export default function Login() {
-  const location = useLocation();
-  console.log("Location : ", location.pathname, "Loc = ", location);
   return <div>Login</div>;
 }
