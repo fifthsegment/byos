6bfca37a8e6c16a7a8add946f576f96c3cc6eddf
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-10-04 11:17:50 +0000

    Update 4 files

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { init } from "../../services/s3";
 
 export default function Home() {
+  const esTest = () => {
+    init();
+  };
   return (
     <div>
       <div>Home</div>
+      <button onClick={esTest}>Test</button>
     </div>
   );
 }
--- src/services/s3.ts
+++ src/services/s3.ts
@@ -1,4 +1,26 @@
 import { Asset } from "./types";
+import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+var AWS = require("aws-sdk");
+var uuid = require("node-uuid");
+
+async function initFn() {
+  const client: any = new S3Client({
+    region: "West",
+    credentials: {
+      accessKeyId: "a",
+      secretAccessKey: "a",
+      expiration: new Date(),
+    },
+    endpoint: "https://s3.us-west-002.backblazeb2.com",
+  });
+  const params = {
+    Bucket: "Bucket_Name",
+    Delimiter: "/",
+    Prefix: "foldername/",
+  };
+  const command = new ListObjectsV2Command(params);
+  const response = await client.send(command);
+}
 
 export const getAssets: (directoryId: string) => Asset[] = (_directoryId) => {
   /**
@@ -12,3 +34,5 @@ export const deleteAsset: (assetId: string) => void = (_assetId) => {
    * Implementation here
    */
 };
+
+export const init = initFn;
