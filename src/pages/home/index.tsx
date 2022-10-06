4c6b66af24c5458ec00212d2cb1577fa6c0d7afd
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-10-06 06:39:29 +0200

    Update index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import {ContextMenu} from "../../components/ContextMenu/index";
 
 export default function Home() {
   return (
     <>
       <div>Home</div>
-      <ContextMenu/>
     </>
   );
 }
