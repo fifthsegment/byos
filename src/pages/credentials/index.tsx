52befe8ae2f44e145917290b36d0929f888e795b
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-10-08 10:56:48 +0000

    Update index.tsx

--- src/pages/credentials/index.tsx
+++ src/pages/credentials/index.tsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { useLinkClickHandler } from 'react-router-dom'
-import { AppModal } from '../../components/AppModal';
-import { SaveCredentialsForm } from '../../components/SaveCredentialsForm';
+import { AppModal } from '../../components/AppModal'
+import { SaveCredentialsForm } from '../../components/SaveCredentialsForm'
+import { redirect } from 'react-router-dom'
 
 export default function Credentials() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false)
 
-    const onClick = useLinkClickHandler("/credentials");
+    const onClick = useLinkClickHandler('/credentials')
     useEffect(() => {
         setIsOpen(true)
     }, [onClick, setIsOpen])
 
     const handleClose = () => {
-        setIsOpen(false);
+        setIsOpen(false)
+        redirect('/')
     }
 
     return (
         <>
-            <AppModal isOpen={isOpen} handleClose={handleClose}><SaveCredentialsForm /></AppModal>
+            <AppModal isOpen={isOpen} handleClose={handleClose}>
+                <SaveCredentialsForm />
+            </AppModal>
         </>
     )
 }
