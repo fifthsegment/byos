4223368c25611b6d52379ce886a84e6e1abb2c09
fifthsegment <abdullah.irfan1@gmail.com>
2022-10-09 15:53:42 +0200

    fixing eslint

--- src/components/ContextMenu/index.tsx
+++ src/components/ContextMenu/index.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-
-import { menuItems } from "./items";
-
 export const ContextMenu = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+  /*
+  const _handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
+  */
 
   return (
     <>
