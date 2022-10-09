bcacaf678f9fbf167c2038f2c310324cb081b753
fifthsegment <abdullah.irfan1@gmail.com>
2022-10-09 21:07:26 +0200

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import './App.css'
 
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { InternalRouteDef } from './routes'
