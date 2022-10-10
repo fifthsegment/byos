94293b33efe355b3d20bf096e8a1ef9a3c94cd6d
Nauman <nauman001@yahoo.com>
2022-10-10 23:19:37 +0200

    added global colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,76 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 //import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import { InternalRouteDef } from './routes'
+import { InternalRouteDef } from './routes';
 //import { Link } from 'react-router-dom'
-import { RoutingContext } from './contexts/routing/RoutingContext'
+import { RoutingContext } from './contexts/routing/RoutingContext';
 import {
-    ApplicationContext,
-    ApplicationState,
-    initialData,
-} from './contexts/application/ApplicationContext'
+  ApplicationContext,
+  ApplicationState,
+  initialData,
+} from './contexts/application/ApplicationContext';
 /*import {
     getApplicationStateLS,
     setApplicationStateLS,
 } from './services/localstorage'*/
 
-import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import Header from './components/Header'
-import MobileNavigation from './components/MobileNavigation'
-import { getApplicationStateLS, setApplicationStateLS } from './services/localstorage'
-import { useGetApplicationStateFromLs } from './hooks/useGetApplicationStateFromLS'
+import { Provider as PaperProvider } from 'react-native-paper';
+import Header from './components/Header';
+import MobileNavigation from './components/MobileNavigation';
+import {
+  getApplicationStateLS,
+  setApplicationStateLS,
+} from './services/localstorage';
+import { useGetApplicationStateFromLs } from './hooks/useGetApplicationStateFromLS';
 import { PortalProvider } from '@gorhom/portal';
+import { theme } from './theme';
 
 function App() {
-    const [routingState] = React.useContext(RoutingContext)
-    const { data: savedApplicationData, isLoaded } = useGetApplicationStateFromLs(initialData);
+  const [routingState] = React.useContext(RoutingContext);
+  const { data: savedApplicationData, isLoaded } =
+    useGetApplicationStateFromLs(initialData);
 
-    const applicationState = useState(savedApplicationData as ApplicationState)
+  const applicationState = useState(savedApplicationData as ApplicationState);
 
-    /*useEffect(() => {
+  /*useEffect(() => {
         applicationState[1](savedApplicationData);
     }, [savedApplicationData])*/
 
-    const [applicationStateData, setApplicationStateData] = applicationState
+  const [applicationStateData, setApplicationStateData] = applicationState;
 
-    useEffect(() => {
-        //setApplicationStateLS({ ...applicationStateData })
-        setApplicationStateData(savedApplicationData);
-    }, [savedApplicationData])
+  useEffect(() => {
+    //setApplicationStateLS({ ...applicationStateData })
+    setApplicationStateData(savedApplicationData);
+  }, [savedApplicationData]);
 
-    useEffect(() => {
-        if (isLoaded) {
-            console.log("[App] Application state was updated = ", applicationStateData)
+  useEffect(() => {
+    if (isLoaded) {
+      console.log(
+        '[App] Application state was updated = ',
+        applicationStateData
+      );
 
-            setApplicationStateLS({ ...applicationStateData })
-        }
-    }, [applicationStateData, isLoaded])
+      setApplicationStateLS({ ...applicationStateData });
+    }
+  }, [applicationStateData, isLoaded]);
 
-    return (
-        <PaperProvider>
-            <PortalProvider>
-                <ApplicationContext.Provider value={applicationState}>
-                    {routingState.isReady &&
-                        routingState.routes.map((route: InternalRouteDef) => {
-                            return (
-                                null
-                            )
-                        })}
+  console.log(theme);
 
-                    <Header title="BYOS" />
-                    <MobileNavigation />
+  return (
+    <PaperProvider theme={theme}>
+      <PortalProvider>
+        <ApplicationContext.Provider value={applicationState}>
+          {routingState.isReady &&
+            routingState.routes.map((route: InternalRouteDef) => {
+              return null;
+            })}
 
-                </ApplicationContext.Provider>
-            </PortalProvider>
-        </PaperProvider>
-    )
+          <Header title="BYOS" />
+          <MobileNavigation />
+        </ApplicationContext.Provider>
+      </PortalProvider>
+    </PaperProvider>
+  );
 }
 
-export default App
-
+export default App;
