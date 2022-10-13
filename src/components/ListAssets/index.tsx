1f5abafb2452c1452e32e7488e801adc4066cc67
Abdullah <abdullah.irfan1@gmail.com>
2022-10-13 21:47:29 +0200

    First

--- src/components/ListAssets/index.tsx
+++ src/components/ListAssets/index.tsx
@@ -1,13 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import {
-  ApplicationContext,
-  ApplicationContextType
-} from '../../contexts/application/ApplicationContext'
-import { useGetAssets } from '../../hooks/useGetAssets'
-import { useS3Client } from '../../hooks/useS3Client'
-import { DataGrid } from '../DataGrid'
-import { ScrollView, StyleSheet, View } from 'react-native'
-
+import { StyleSheet, View } from 'react-native'
 import {
   Text,
   ActivityIndicator,
@@ -15,9 +7,17 @@ import {
   IconButton,
   Card
 } from 'react-native-paper'
+import { Portal } from '@gorhom/portal'
+import {
+  ApplicationContext,
+  ApplicationContextType
+} from '../../contexts/application/ApplicationContext'
+import { useGetAssets } from '../../hooks/useGetAssets'
+import { useS3Client } from '../../hooks/useS3Client'
+import { DataGrid } from '../DataGrid'
+
 import { Asset } from '../../services/types'
 import { GetAssetArgs } from '../../services/s3/types'
-import { Portal } from '@gorhom/portal'
 import { Block } from '../../services/rn-responsive-design'
 
 export const ListAssets: React.FC = () => {
@@ -62,67 +62,72 @@ export const ListAssets: React.FC = () => {
   }
 
   return (
-    <>
-      {s3Initialized
-        ? <>
-          <Portal hostName="Reloader">
-            <IconButton
-              animated={true}
-              icon="reload"
-              onPress={() => {
-                setRerun(`${Math.random()}`)
-              }}
-            />
-          </Portal>
-          <Portal hostName="Back">
-            {dataQuery.Prefix?.length > 0 && (
-              <IconButton
-                icon="arrow-left"
-                onPress={() => {
-                  goBack()
-                }}
-              />
+        <View>
+            {s3Initialized
+              ? (
+                <>
+                    <Portal hostName="Reloader">
+                        <IconButton
+                            animated
+                            icon="reload"
+                            onPress={() => {
+                              setRerun(`${Math.random()}`)
+                            }}
+                        />
+                    </Portal>
+                    <Portal hostName="Back">
+                        {dataQuery.Prefix?.length > 0 && (
+                            <IconButton
+                                icon="arrow-left"
+                                onPress={() => {
+                                  goBack()
+                                }}
+                            />
+                        )}
+                    </Portal>
+                    <View style={styles.root}>
+                        <View style={styles.section1}>
+                            <Text variant="bodyLarge" style={styles.path}>
+                                {`Bucket Root /${dataQuery.Prefix}`}
+                                <AnimatedFAB
+                                    icon="plus"
+                                    label="Label"
+                                    extended={isExtended}
+                                    onPress={() => console.log('Pressed')}
+                                    visible
+                                    animateFrom="right"
+                                    iconMode="static"
+                                    style={[styles.fabStyle]}
+                                />
+                            </Text>
+                            {isLoading && <ActivityIndicator animating />}
+                            {isError && (
+                                <Text variant="headlineSmall">Error </Text>
+                            )}
 
-            )}
-          </Portal>
-
-          <View style={styles.root}>
-            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-              <View style={styles.section1}>
-                <Text variant="bodyLarge" style={styles.path}>
-                  {`Bucket Root /${dataQuery.Prefix}`}
-                </Text>
-                {isLoading && <ActivityIndicator animating={true} />}
-                {isError && <Text variant="headlineSmall">Error </Text>}
-                <AnimatedFAB
-                  icon={'plus'}
-                  label={'Label'}
-                  extended={isExtended}
-                  onPress={() => console.log('Pressed')}
-                  visible={true}
-                  animateFrom={'right'}
-                  iconMode={'static'}
-                  style={[styles.fabStyle]}
-                />
-                {(data != null) && !isLoading && (
-                  <DataGrid assets={data} onPress={onPress} />
+                            {data != null && !isLoading && (
+                                <DataGrid assets={data} onPress={onPress} />
+                            )}
+                        </View>
+                        <Block hidden={['xs', 'md']}>
+                            <View style={styles.section2}>
+                                <Text variant="bodyLarge" style={styles.path}>
+                                    Preview pane
+                                </Text>
+                            </View>
+                        </Block>
+                    </View>
+                </>
+                )
+              : (
+                <Card style={styles.errorMessage}>
+                    <Text>
+                        S3 Client has not been initialized, please update your
+                        API Configuration first.
+                    </Text>
+                </Card>
                 )}
-              </View>
-            </ScrollView>
-            <Block hidden={['xs', 'md']}>
-              <View style={styles.section2}>
-                <Text variant="bodyLarge" style={styles.path}>
-                  {'Preview pane'}
-                </Text>
-              </View>
-            </Block>
-          </View>
-
-        </>
-        : <Card style={styles.errorMessage}>
-          <Text>S3 Client has not been initialized, please update your API Configuration first.</Text>
-        </Card>}
-    </>
+        </View>
   )
 }
 
@@ -131,8 +136,8 @@ const styles = StyleSheet.create({
     margin: 10
   },
   fabStyle: {
-    bottom: 16,
-    right: 16,
+    bottom: 45,
+    right: 25,
     position: 'absolute',
     zIndex: 1
   },
