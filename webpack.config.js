d1e44149eb02ab58551c6da9b5e98c6adba075d8
Abdullah <abdullah.irfan1@gmail.com>
2022-10-23 13:31:30 +0200

    Add support for PWA

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config')
 
 // Expo CLI will await this method so you can optionally return a promise.
 module.exports = async function (env, argv) {
+
+  const isEnvProduction = env.mode === "production";
+  
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
@@ -17,6 +20,29 @@ module.exports = async function (env, argv) {
     },
     argv
   )
+
+  if (isEnvProduction) {
+    config.plugins.push(
+      // Generate a service worker script that will precache, and keep up to date,
+      // the HTML & assets that are part of the webpack build.
+      new WorkboxWebpackPlugin.InjectManifest({
+        swSrc: path.resolve(__dirname, "src/service-worker.js"),
+        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+        exclude: [
+          /\.map$/,
+          /asset-manifest\.json$/,
+          /LICENSE/,
+          /\.js\.gz$/,
+          // Exclude all apple touch and chrome images because they're cached locally after the PWA is added.
+          /(apple-touch-startup-image|chrome-icon|apple-touch-icon).*\.png$/,
+        ],
+        // Bump up the default maximum size (2mb) that's precached,
+        // to make lazy-loading failure scenarios less likely.
+        // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      })
+    );
+  }
 
   // Finally return the new config for the CLI to use.
   return config
